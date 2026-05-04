732e2ca3004c10bb61c7cec36f977d77cdb8b45a
yishant11 <yishant11@example.com>
2026-05-04 15:09:53 +0530

    url fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@
 // const BASE_URL = "/api";
 
 
-const BASE_URL = location.hostname === "localhost" ? "http://localhost:3002" : "/api";
+const BASE_URL = location.hostname === "localhost" ? "http://localhost:3002/api" : "/api";
 
 export default BASE_URL;
